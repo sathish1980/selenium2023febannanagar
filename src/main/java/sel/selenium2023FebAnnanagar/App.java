c3a24e26f9dfdf81fdef9ef151e5109527956b34
satish1980 <kumar.sathish189@gmail.com>
2023-03-14 23:02:19 +0530

    new project

---
+++ src/main/java/sel/selenium2023FebAnnanagar/App.java
@@ -0,0 +1,13 @@
+package sel.selenium2023FebAnnanagar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sel/selenium2023FebAnnanagar/SAmplecalss.java
@@ -0,0 +1,10 @@
+package sel.selenium2023FebAnnanagar;
+
+public class SAmplecalss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
